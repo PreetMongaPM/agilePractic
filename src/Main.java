6fc59bc5f692b5c1c8d7d30908c6debe55608f10
Preet Monga <131587235+PreetMongaPM@users.noreply.github.com>
2024-04-23 17:42:15 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,6 @@ import java.lang.Math;
 public class Main {
     public static void main(String [] args)
     {
-        int a,b,c;
-        System.out.println("Enter the coefficients a,b,c of quadratic equations");
-        Scanner in = new Scanner(System.in);
-        a = in.nextInt();
-        b = in.nextInt();
-        c = in.nextInt();
-
-        int D = b*b - 4*a*c;
-
-        if(D>0)
-        {
-            System.out.println("Both roots are distinct :"+((-b+sqrt(D))/2*a)+" and "+(-b-sqrt(D))/2*a));
-        }
-        else if(D==0)
-        {
-            System.out.println("Both roots are equal :"+((-b+sqrt(D))/2*a)+" and "+(-b-sqrt(D))/2*a));
-        }
-        else{
-            System.out.println("Roots are imaginary");
-        }
-
+        System.out.println("Hello My name is PM");
     }
 }
